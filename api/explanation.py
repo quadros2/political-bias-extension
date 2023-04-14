58bc83bdc6a4ac935de06a29efcc9d941cba1dce
Rohit Narayanan <rohitmen01@gmail.com>
2023-04-14 15:17:57 -0500

    added some starter code

--- api/explanation.py
+++ api/explanation.py
@@ -1,9 +1,34 @@
+from chatgpt_wrapper import ChatGPT
+
 def get_explainability_prompt(data):
     return (
-        "Please identify certain phrases in an article that may indicate"
-        f"{data['bias']}-wing political bias from an article with this"
+        "Please identify certain phrases in an article that may indicate "
+        f"{data['bias']}-wing political bias from an article with this "
         f"link: {data['url']}"
     )
 
 def postprocess_explaination(explanation):
     return explanation
+
+
+def explaination(data):
+    bot = ChatGPT()
+    print(data)
+    prompt = get_explainability_prompt(data)
+    print(prompt)
+    ok, response, _ = bot.ask(prompt)
+    
+
+    bot._shutdown()
+
+    return postprocess_explaination(response)
+
+
+data = {
+    'bias': 'left',
+    'url': 'https://www.cnn.com/2023/04/14/politics/nra-convention-republican-2024-presidential-race/index.html'
+}
+
+out = explaination(data)
+
+print(out)
